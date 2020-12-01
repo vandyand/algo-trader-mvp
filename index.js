1c0c6b23d0951a7a66cdbd1998bb77ff0f7e2eeb
vandyand <vandyand@gmail.com>
2020-11-30 21:21:21 -0500

    wip: adding take profit and stop loss functionality

--- index.js
+++ index.js
@@ -3,18 +3,19 @@ const env = require("dotenv").config();
 
 const clArgs = process.argv.slice(2);
 let func = clArgs.length > 0 ? clArgs[0] : "getAccountIds";
-let account = "101-001-5729740-010";
+// let account = "101-001-5729740-010";
+let account = "101-001-5729740-002";
 let units = "0";
 
-if(clArgs.length > 1){
-    if(clArgs[1].length == 19){
-        account = clArgs[1];
-    } else {
-        units = clArgs[1];
-    }
+if (clArgs.length > 1) {
+  if (clArgs[1].length == 19) {
+    account = clArgs[1];
+  } else {
+    units = clArgs[1];
+  }
 }
-if(clArgs.length > 2 && units == "0"){
-    units = clArgs[2];
+if (clArgs.length > 2 && units == "0") {
+  units = clArgs[2];
 }
 
 const url = "https://api-fxpractice.oanda.com/v3/accounts";
@@ -39,33 +40,63 @@ const getAccountSummary = () => {
     .catch((err) => console.log(err));
 };
 
-const executeOrder = () => {
-  const body = {
+const executeOrder = (
+  body = {
     order: {
       units,
       instrument: "EUR_USD",
+      // instrument: "USD_CAD",
+      // instrument: "GBP_USD",
       timeInForce: "FOK",
       type: "MARKET",
       positionFill: "DEFAULT",
     },
-  };
+  }
+) => {
   axios({
     method: "post",
     url: `${url}/${account}/orders`,
     headers,
     data: body,
   })
-    .then((res) => console.log(res.data))
+    .then((res) => {
+      console.log(res.data)
+      return res.data;
+    })
     .catch((err) => console.log(err));
 };
 
+const addTPSLOrder = ()
+
 const getPositions = () => {
-  axios
-    .get(`${url}/${account}/positions`, {
+  return axios
+    .get(`${url}/${account}/openPositions`, {
       headers,
     })
-    .then((res) => console.log(res.data.positions))
+    .then((res) => {
+      console.log(res.data.positions);
+      return res.data.positions;
+    })
     .catch((err) => console.log(err));
+};
+
+const closeAllPositions = () => {
+  getPositions().then((positions) => {
+    positions.map((position) => {
+      const units = Number(position.long.units) + Number(position.short.units);
+      if (units != 0) {
+        executeOrder({
+          order: {
+            units: units * -1,
+            instrument: position.instrument,
+            timeInForce: "FOK",
+            type: "MARKET",
+            positionFill: "DEFAULT",
+          },
+        });
+      }
+    });
+  });
 };
 
 switch (func) {
@@ -77,6 +108,9 @@ switch (func) {
     break;
   case "getPositions":
     getPositions();
+    break;
+  case "closeAllPositions":
+    closeAllPositions();
     break;
   default:
     getAccountIds();
