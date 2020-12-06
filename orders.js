339933966d242ce2e7c321b73b2b39213dc40756
vandyand <vandyand@gmail.com>
2020-12-06 01:15:36 -0500

    chore: refactoring

--- orders.js
+++ orders.js
@@ -1,33 +1,9 @@
 import axios from "axios";
-import { getOrdersToExecute } from "./db.js";
-import yargs from "yargs";
-import dotenv from "dotenv";
-import { getDefaultAccountId, updateDefaultAccountId } from "./db.js";
+import { getOrdersToExecute, getDefaultAccountId, getBaseUrl, getHeaders } from "./db.js";
 
-
-// var argv = require('yargs/yargs')(process.argv.slice(2))
-var argv = yargs(process.argv.slice(2))
-  .usage('Usage: $0 -f [func] -a [str] -i [str] -u [num] -r [num]')
-  .alias("f", "function")
-  .alias("a", "account-id")
-  .alias("i", "instrument")
-  .alias("u", "units")
-  .alias("r", "range")
-  .demandOption(['f'])
-  .argv;
-
-const func = argv.f;
-const accountId = argv.a || getDefaultAccountId();
-const instrument = argv.i || 'EUR_USD';
-const units = Number(argv.u) || 0;
-const range = Number(argv.r) || 0;
-
-const env = dotenv.config();
-const url = "https://api-fxpractice.oanda.com/v3/accounts";
-const headers = {
-  Authorization: `Bearer ${process.env.OANDA_API_KEY}`,
-  "Content-Type": "application/json",
-};
+const accountId = getDefaultAccountId();
+const url = getBaseUrl();
+const headers = getHeaders();
 
 const createMarketOrderObj = (instrument = "EUR_USD", units = 0) => {
   return {
